9517d0e7a91d39b8b4f77207887ad925119eec8b
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-04-10 14:24:42 +0200

    little cleanup

--- DeviceImplementations/EDeviceImplementation.cs
+++ DeviceImplementations/EDeviceImplementation.cs
@@ -7,19 +7,12 @@ using ECore.DataSources;
 
 namespace ECore
 {
-    //abstract class for all device-specific implementations
-    //all new HW devices should inherit from this class
-    //groups all information in memories and functionalities
     abstract public class EDeviceImplementation
     {
         //////////////////////////////////////////////////////////////////
-        //private properties        
-        //private Dictionary<Type, List<object>> sortedInterfaceDictionary;
-
-        //////////////////////////////////////////////////////////////////
         //shared properties        
         protected EDevice eDevice;
-        //FIXME: make me protected
+        //FIXME: make me protected in release mode
         public EDeviceHWInterface hardwareInterface;
         protected List<DeviceMemory<MemoryRegister<byte>>> byteMemories;
         protected List<DataSource> dataSources;
@@ -32,8 +25,6 @@ namespace ECore
         abstract public void InitializeDataSources();
         abstract public bool Start();
         abstract public void Stop();
-        //FIXME: these are too specific for a "Device" -> either call it scope or move them down to the scope
-        //abstract public DeviceImplementations.ScopeV2.ScopeV2RomManager CreateRomManager();
 
         //////////////////////////////////////////////////////////////////
         //base functionality implementation
@@ -41,53 +32,13 @@ namespace ECore
         {
             this.eDevice = eDevice;
 
-            //automatically calls the methods which need to be executed during initialization
             byteMemories = new List<DeviceMemory<MemoryRegister<byte>>>();
             InitializeMemories();
             dataSources = new List<DataSource>();
             InitializeDataSources();
         }
 
-        //getters
         virtual public List<DeviceMemory<MemoryRegister<byte>>> Memories { get { return byteMemories; } }
-        /*
-        public virtual List<object> GetInterfaces(Type interfaceType)
-        {
-            //if this method is called the first time: init list
-            if (this.sortedInterfaceDictionary == null)
-                InitSortedInterfaceDictionary();
-
-            //if the interface is implemented, return it. otherwise, return null
-            if (sortedInterfaceDictionary.ContainsKey(interfaceType))
-                return sortedInterfaceDictionary[interfaceType];
-            else
-                return null;
-        }*/
-        /*
-        private void InitSortedInterfaceDictionary()
-        {
-            this.sortedInterfaceDictionary = new Dictionary<Type, List<object>>();
-
-            //scroll over all implemented interfaces
-            foreach (object functionality in this.functionalities)
-            {
-                Type currentType = functionality.GetType();
-                Type[] iList = currentType.FindInterfaces(new System.Reflection.TypeFilter(MyInterfaceFilter), null);
-                Type currentInterface = iList[0];
-
-                //if dictionary does not contains a list for this type of funcs: create and add
-                if (!sortedInterfaceDictionary.ContainsKey(currentType)) 
-                    sortedInterfaceDictionary.Add(currentInterface, new List<object>());
-
-                //now add this functionality to the correct list
-                sortedInterfaceDictionary[currentInterface].Add(functionality);//if dictionary does not contain a list for this type of funcs: create
-            }
-        }*/
-
-        private bool MyInterfaceFilter(Type typeObj, Object criteriaObj)
-        {
-            return true;
-        }
 
         public bool HasSetting(Setting s) 
         {
