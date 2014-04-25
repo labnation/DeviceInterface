eb83a90a914b6e40327f8856bcb8ec5c345f9954
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-04-25 18:20:14 +0200

    ScopeDummy: Read matfile once, massively reduces CPU consumption

--- Devices/ScopeDummyFile.cs
+++ Devices/ScopeDummyFile.cs
@@ -9,21 +9,27 @@ namespace ECore.Devices
     partial class ScopeDummy
     {
         private static string sequenceFilename = "i2c_sequence.mat";
+        private static float[] readChannelA = null;
+        private static float[] readChannelB = null;
+        private static double[] readTime = null;
 
         public static bool GetWaveFromFile(TriggerMode triggerMode, double triggerHoldoff, uint triggerChannel, TriggerDirection triggerDirection, float triggerLevel, uint decimation, double samplePeriod, ref float[][] output)
         {
             string filename = Utils.ApplicationDataPath + sequenceFilename;
-            MatfileReader matfileReader = new MatlabFileIO.MatfileReader(filename);
-            
+            if(readChannelA == null || readChannelB == null || readTime == null) 
+            {
+                MatfileReader matfileReader = new MatlabFileIO.MatfileReader(filename);
+                readChannelA = Utils.CastArray<double, float>(matfileReader.Variables["chA"].data as double[]);
+                readChannelB = Utils.CastArray<double, float>(matfileReader.Variables["chB"].data as double[]);
+                readTime = matfileReader.Variables["time"].data as double[];
+                matfileReader.Close();
+            }
+    
             float[][] waves = new float[2][];
 
-            waves[0] = Utils.DecimateArray(Utils.CastArray<double, float>(matfileReader.Variables["chA"].data as double[]), decimation);
-
-            waves[1] = Utils.DecimateArray(Utils.CastArray<double, float>(matfileReader.Variables["chB"].data as double[]), decimation);
-            
-            double[] time = matfileReader.Variables["time"].data as double[];
-
-            matfileReader.Close();
+            waves[0] = Utils.DecimateArray(readChannelA, decimation);
+            waves[1] = Utils.DecimateArray(readChannelB, decimation);
+            double[] time = readTime;
 
             double samplePeriodMeasured = time[decimation] - time[0];
             if (samplePeriodMeasured != samplePeriod)
