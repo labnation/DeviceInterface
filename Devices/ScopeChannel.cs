933f5deb6c9a4960db156ef9e4d5ddb3ff9a7183
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-04-29 16:02:25 +0200

    Use shorted names for analog channels, converted List of channels to HashSet to ensure uniqueness

--- Devices/ScopeChannel.cs
+++ Devices/ScopeChannel.cs
@@ -18,8 +18,8 @@ namespace ECore.Devices
         public static DigitalChannel Digi6 = new DigitalChannel("Digital 6", 6);
         public static DigitalChannel Digi7 = new DigitalChannel("Digital 7", 7);
 
-        public static AnalogChannel ChA  = new AnalogChannel("Channel A", 0);
-        public static AnalogChannel ChB  = new AnalogChannel("Channel B", 1);
+        public static AnalogChannel ChA  = new AnalogChannel("Ch A", 0);
+        public static AnalogChannel ChB  = new AnalogChannel("Ch B", 1);
         public static AnalogChannel Math = new AnalogChannel("Math", 2);
     }
 
@@ -33,7 +33,7 @@ namespace ECore.Devices
         public string Name { get; protected set; }
         public int Value { get; protected set; }
         public int Order { get; protected set; }
-        public static List<ScopeChannel> list = new List<ScopeChannel>();
+        public static HashSet<ScopeChannel> list = new HashSet<ScopeChannel>();
         public ScopeChannel(string name, int value)
         {
             this.Name = name; 
@@ -45,15 +45,15 @@ namespace ECore.Devices
     }
     public class AnalogChannel : ScopeChannel 
     {
-        new public static List<AnalogChannel> list = new List<AnalogChannel>();
+        new public static HashSet<AnalogChannel> list = new HashSet<AnalogChannel>();
         public AnalogChannel(string name, int value) : base(name, value) { list.Add(this); } 
     }
     public class DigitalChannel : ScopeChannel {
-        new public static List<DigitalChannel> list = new List<DigitalChannel>();
+        new public static HashSet<DigitalChannel> list = new HashSet<DigitalChannel>();
         public DigitalChannel(string name, int value) : base(name, value) { list.Add(this); } 
     }
     public class ProtocolChannel : ScopeChannel {
-        new public static List<ProtocolChannel> list = new List<ProtocolChannel>();
+        new public static HashSet<ProtocolChannel> list = new HashSet<ProtocolChannel>();
         public ProtocolChannel(string name, int value) : base(name, value) { list.Add(this);  } 
     }
 }
