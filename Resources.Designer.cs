804abfa8b327160d1c3a0289d2f0b274603b890a
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-08-19 22:23:17 +0200

    Added A12 Firmware

--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace ECore {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] SmartScope_A12 {
+            get {
+                object obj = ResourceManager.GetObject("SmartScope_A12", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
