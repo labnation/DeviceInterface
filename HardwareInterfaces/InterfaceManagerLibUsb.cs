3e65aa59b25b4a8382d739aa53d489bd93ccce3e
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-10-15 13:55:43 +0200

    Removed some useless code

--- HardwareInterfaces/InterfaceManagerLibUsb.cs
+++ HardwareInterfaces/InterfaceManagerLibUsb.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-//#if IPHONE
-//#else
 using LibUsbDotNet;
 using LibUsbDotNet.Main;
 using LibUsbDotNet.DeviceNotify;
 using C=Common;
-//#endif
 
 namespace ECore.HardwareInterfaces
 {
