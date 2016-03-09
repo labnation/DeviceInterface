423e2d1ff8f05d73c43b4957e105c0a3d3a47951
Jasper van Bourgognie <van.bourgognie@gmail.com>
2016-03-09 12:54:25 +0100

    remove dependency on LabNation.Interfaces

--- DataSources/DataStorage/ChannelBuffer.cs
+++ DataSources/DataStorage/ChannelBuffer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using LabNation.Interfaces;
 using LabNation.DeviceInterface.Devices;
 using System.Runtime.Serialization.Formatters.Binary;
 
