cc42c8e2b3ca98133b8f106fb41e1792c0b2151d
Jasper van Bourgognie <van.bourgognie@gmail.com>
2016-06-10 07:12:31 +0200

    Add Server console exec

---
+++ SmartScopeServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabNation.Common;
+using LabNation.DeviceInterface.Net;
+using System.IO;
+
+namespace SmartScopeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            ConsoleLogger consoleLog = new ConsoleLogger(LogLevel.DEBUG);
+
+            Server server = new Server();
+
+            Logger.LogC(LogLevel.INFO, "--- Press any key to stop server ---\n", ConsoleColor.Green);
+            Console.ReadKey();
+
+            server.Stop();
+        }        
+    }
+}
