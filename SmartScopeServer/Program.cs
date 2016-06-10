e6fe6bd9e3125975eeb928c313c531fd44226693
Jasper van Bourgognie <van.bourgognie@gmail.com>
2016-06-11 01:13:04 +0200

    SmartScopeServer: fix winusb events

--- SmartScopeServer/Program.cs
+++ SmartScopeServer/Program.cs
@@ -6,21 +6,34 @@ using System.Threading.Tasks;
 using LabNation.Common;
 using LabNation.DeviceInterface.Net;
 using System.IO;
+#if WINDOWS
+using System.Windows.Forms;
+#endif
 
 namespace SmartScopeServer
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
-        {            
+        {
             ConsoleLogger consoleLog = new ConsoleLogger(LogLevel.DEBUG);
 
-            Server server = new Server();
+            Monitor interfaceMonitor = new Monitor();
 
             Logger.LogC(LogLevel.INFO, "--- Press any key to stop server ---\n", ConsoleColor.Green);
-            Console.ReadKey();
-
-            server.Stop();
+            
+#if WINDOWS
+            //Need the Application thread to enable winusb device detection
+            Application.EnableVisualStyles();
+            while (true)
+            {
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(10);
+            }
+#endif
+            
+            interfaceMonitor.Stop();
 
             consoleLog.Stop();
         }        
