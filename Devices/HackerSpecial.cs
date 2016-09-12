9fcb3dc68510db30170b8da1b7b6361fe31a6d04
Jasper van Bourgognie <van.bourgognie@gmail.com>
2016-09-12 11:50:22 +0200

    HackerSpecial: add ADC configuration

--- Devices/HackerSpecial.cs
+++ Devices/HackerSpecial.cs
@@ -23,7 +23,7 @@ namespace LabNation.DeviceInterface.Devices
         public string Serial { get; private set; }
 
         public ScopeFpgaRom FpgaRom { get; private set; }
-        public ScopeFpgaSettingsMemory FpgaSettingsMemory { get; private set; }
+        public ScopeFpgaI2cMemory FpgaSettingsMemory { get; private set; }
         public ScopeFpgaI2cMemory FpgaUserMemory { get; private set; }
         public HackerSpecial(ISmartScopeInterface iface)
         {
@@ -36,7 +36,7 @@ namespace LabNation.DeviceInterface.Devices
             byte FPGA_I2C_ADDRESS_ROM = 0x0D;
             byte FPGA_I2C_ADDRESS_USER = 0x0E;
 
-            FpgaSettingsMemory = new ScopeFpgaSettingsMemory(iface, FPGA_I2C_ADDRESS_SETTINGS);
+            FpgaSettingsMemory = new ScopeFpgaI2cMemory(iface, FPGA_I2C_ADDRESS_SETTINGS, 39);
             FpgaRom = new ScopeFpgaRom(iface, FPGA_I2C_ADDRESS_ROM);
             FpgaUserMemory = new ScopeFpgaI2cMemory(iface, FPGA_I2C_ADDRESS_USER, 256);
 
@@ -69,6 +69,46 @@ namespace LabNation.DeviceInterface.Devices
         public byte[] GetFpgaData()
         {
             return this.iface.GetData(64);
+        }
+
+        public void ConfigureAdc()
+        {
+            WriteAdcReg(0x0A, 0x5A); //SW reset
+            WriteAdcReg(0x00, 0x04);
+            WriteAdcReg(0x02, 0x00);
+            WriteAdcReg(0x06, 16);
+            WriteAdcReg(0x03, 56);
+            WriteAdcReg(0x04, 0x02);
+            WriteAdcReg(0x00, 0x03);
+            WriteAdcReg(0x01, 0x02);
+        }
+
+        const uint REG_SPI_ADDR = 1;
+        const uint REG_SPI_VAL = 2;
+        const uint REG_FLAGS = 5;
+        const byte REG_FLAGS_SPI_MASK = 0x01;
+        const uint ROM_SPI_VAL = 4;
+
+        public byte ReadAdcReg(uint address)
+        {
+            FpgaSettingsMemory[REG_SPI_ADDR].WriteImmediate((byte)(address + 128)); //for a read, MSB must be 1
+
+            //next, trigger rising edge to initiate SPI comm
+            FpgaSettingsMemory[REG_FLAGS].WriteImmediate(0);
+            FpgaSettingsMemory[REG_FLAGS].WriteImmediate(REG_FLAGS_SPI_MASK);
+
+            //finally read acquired value
+            return FpgaRom[ROM_SPI_VAL].Read().GetByte();
+        }
+
+        public void WriteAdcReg(uint address, byte value)
+        {
+            FpgaSettingsMemory[REG_SPI_ADDR].WriteImmediate((byte)(address)); //for a read, MSB must be 1
+
+            FpgaSettingsMemory[REG_SPI_VAL].WriteImmediate(value);
+            //next, trigger rising edge to initiate SPI comm
+            FpgaSettingsMemory[REG_FLAGS].WriteImmediate(0);
+            FpgaSettingsMemory[REG_FLAGS].WriteImmediate(REG_FLAGS_SPI_MASK);
         }
 
         public IHardwareInterface HardwareInterface { get { return this.iface; } }
