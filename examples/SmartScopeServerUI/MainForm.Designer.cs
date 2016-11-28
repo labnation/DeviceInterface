b7153187076242471079c9d172e2b28309773580
Jasper van Bourgognie <van.bourgognie@gmail.com>
2016-11-28 09:29:51 +0100

    Add Server UI

---
+++ examples/SmartScopeServerUI/MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿using System.Windows.Forms;
+
+namespace LabNation.SmartScopeServerUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.logbox = new System.Windows.Forms.RichTextBox();
+            this.status = new System.Windows.Forms.StatusStrip();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.splitter)).BeginInit();
+            this.splitter.Panel1.SuspendLayout();
+            this.splitter.Panel2.SuspendLayout();
+            this.splitter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitter
+            // 
+            this.splitter.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitter.Location = new System.Drawing.Point(0, 27);
+            this.splitter.Name = "splitter";
+            // 
+            // splitter.Panel1
+            // 
+            this.splitter.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitter.Panel2
+            // 
+            this.splitter.Panel2.Controls.Add(this.logbox);
+            this.splitter.Size = new System.Drawing.Size(1039, 402);
+            this.splitter.SplitterDistance = 400;
+            this.splitter.TabIndex = 5;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 86F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(394, 396);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // logbox
+            // 
+            this.logbox.BackColor = System.Drawing.Color.Black;
+            this.logbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.logbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logbox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logbox.ForeColor = System.Drawing.Color.White;
+            this.logbox.Location = new System.Drawing.Point(0, 0);
+            this.logbox.Name = "logbox";
+            this.logbox.ReadOnly = true;
+            this.logbox.Size = new System.Drawing.Size(635, 402);
+            this.logbox.TabIndex = 1;
+            this.logbox.Text = "";
+            // 
+            // status
+            // 
+            this.status.Location = new System.Drawing.Point(0, 432);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(1039, 22);
+            this.status.TabIndex = 1;
+            this.status.Text = "statusStrip1";
+            // 
+            // menu
+            // 
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(1039, 24);
+            this.menu.TabIndex = 2;
+            this.menu.Text = "menuStrip1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 454);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.splitter);
+            this.MainMenuStrip = this.menu;
+            this.Name = "MainForm";
+            this.Text = "SmartScope Network Server";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.splitter.Panel1.ResumeLayout(false);
+            this.splitter.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitter)).EndInit();
+            this.splitter.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitter;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.StatusStrip status;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.RichTextBox logbox;
+    }
+}
+
---
+++ examples/SmartScopeServerUI/MainForm.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LabNation.Common;
+using LabNation.DeviceInterface.Net;
+using System.Collections.Concurrent;
+using System.Threading;
+using LabNation.DeviceInterface.Hardware;
+using LabNation.DeviceInterface.Devices;
+
+namespace LabNation.SmartScopeServerUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<InterfaceServer, int> tableRows = new Dictionary<InterfaceServer, int>();
+        ConcurrentQueue<LogMessage> logqueue = new ConcurrentQueue<LogMessage>();
+        Thread logThread;
+        LabNation.DeviceInterface.Net.Monitor interfaceMonitor;
+        bool running = true;
+        
+        private void Form_Load(object sender, EventArgs e)
+        {
+            Logger.AddQueue(logqueue);
+            logThread = new Thread(DequeueLog);
+            logThread.Name = "Logbox";
+            logThread.Start();
+            this.FormClosing += Cleanup;
+            Logger.Debug("App started");
+            interfaceMonitor = new LabNation.DeviceInterface.Net.Monitor(false, OnServerChanged);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Serial" }, 0, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Port" }, 1, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Connected" }, 2, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Btns" }, 3, 0);
+
+            ToolStripMenuItem clearLog = new ToolStripMenuItem()
+            {
+                Text = "Clear log",
+            };
+            clearLog.Click += delegate { BeginInvoke((MethodInvoker)delegate { logbox.Clear(); }); };
+            menu.Items.Add(clearLog);
+        }
+
+        private void Cleanup(object sender, FormClosingEventArgs e)
+        {
+            running = false;
+            logThread.Join(1000);
+            if (logThread.IsAlive)
+                logThread.Abort();
+            Logger.Info("Stopping interface monitor");
+            interfaceMonitor.Stop();
+
+            Logger.Info("Stopping console logger");
+            
+        }
+
+        private void OnServerChanged(InterfaceServer s, bool connected)
+        {
+            if(connected)
+            {
+                BeginInvoke((MethodInvoker)delegate { AddServerToTable(s); });
+            } else
+            {
+                BeginInvoke((MethodInvoker)delegate { RemoveServerFromTable(s); });
+            }
+
+        }
+
+        private void AddServerToTable(InterfaceServer s)
+        {
+            if(tableRows.ContainsKey(s))
+            {
+                //Update
+                int row = tableRows[s];
+                Label labelPort = (Label)tableLayoutPanel1.GetControlFromPosition(1, row);
+                Button startButton = (Button)tableLayoutPanel1.GetControlFromPosition(3, row);
+                startButton.Text = "Stop";
+                startButton.Click += StopServer;
+                startButton.Enabled = true;
+                labelPort.Text = s.Port.ToString();
+            }
+            else
+            {
+                Label labelSerial = new Label() { Text = s.hwInterface.Serial, Tag = s };
+                Label labelPort = new Label() { Text = s.Port.ToString(), Tag = s };
+                Button startButton = new Button()
+                {
+                    Text = "start",
+                    Tag = s,
+                    Dock = DockStyle.Fill,
+
+                };
+                startButton.Click += StartServer;
+
+                tableLayoutPanel1.RowCount += 1;
+                tableLayoutPanel1.RowStyles.Add(new RowStyle());
+                int row = tableLayoutPanel1.RowCount - 2;
+                tableLayoutPanel1.Controls.Add(labelSerial, 0, row);
+                tableLayoutPanel1.Controls.Add(labelPort, 1, row);
+                tableLayoutPanel1.Controls.Add(startButton, 3, row);
+                tableRows[s] = row;
+            }
+
+        }
+
+        private void StartServer(object s, EventArgs e) {
+            Button b = (Button)s;
+            b.Click -= StartServer;
+            b.Enabled = false;
+            ((InterfaceServer)b.Tag).Start();
+        }
+        private void StopServer(object s, EventArgs e) { ((InterfaceServer)((Control)s).Tag).Stop(); }
+        private void RemoveServerFromTable(InterfaceServer s)
+        {
+            //Find row where this server lives
+            int row = tableRows[s];
+            tableRows.Remove(s);
+
+            // delete all controls of row that we want to delete
+            for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+            {
+                var control = tableLayoutPanel1.GetControlFromPosition(i, row);
+                tableLayoutPanel1.Controls.Remove(control);
+            }
+
+            // move up row controls that comes after row we want to remove
+            for (int i = row + 1; i < tableLayoutPanel1.RowCount; i++)
+            {
+                for (int j = 0; j < tableLayoutPanel1.ColumnCount; j++)
+                {
+                    var control = tableLayoutPanel1.GetControlFromPosition(j, i);
+                    if (control != null)
+                    {
+                        tableLayoutPanel1.SetRow(control, i - 1);
+                    }
+                }
+            }
+
+            // remove last row
+            tableLayoutPanel1.RowStyles.RemoveAt(tableLayoutPanel1.RowCount - 1);
+            tableLayoutPanel1.RowCount--;
+
+        }
+
+        private void AddLogMessage(LogMessage m)
+        {
+            if (InvokeRequired) {
+                this.Invoke((MethodInvoker)delegate { AddLogMessage(m); });
+                return;
+            }
+            ConsoleColor c = m.color.HasValue ? m.color.Value : ConsoleColor.Green;
+            string msg = String.Format("[{0}] {1} {2}", m.level.ToString().ToUpper(), m.message, Environment.NewLine);
+
+            var box = logbox;
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = Color.FromName(c.ToString());
+            box.AppendText(msg);
+            box.SelectionColor = box.ForeColor;
+            box.ScrollToCaret();
+        }
+
+        private void DequeueLog()
+        {
+            while(running)
+            {
+                Thread.Sleep(50);
+                LogMessage m;
+                while (logqueue.TryDequeue(out m))
+                    AddLogMessage(m);
+            }
+        }
+    }
+}
