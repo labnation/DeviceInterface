26af1aa6d921e9ce671f044a0871690f1bd75e03
Jasper van Bourgognie <van.bourgognie@gmail.com>
2016-12-15 16:15:26 +0100

    Fix Mac Menu bar

--- examples/SmartScopeServerUI/AppDelegate.designer.cs
+++ examples/SmartScopeServerUI/AppDelegate.designer.cs
@@ -1,10 +1,32 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
 
 namespace LabNation.SmartScopeServerUI
 {
-	// Should subclass MonoMac.AppKit.NSResponder
-	[Foundation.Register ("AppDelegate")]
-	public partial class AppDelegate
+	[Register ("AppDelegate")]
+	partial class AppDelegate
 	{
+		[Outlet]
+		AppKit.NSMenuItem autostartitem { get; set; }
+
+		[Action ("autostart:")]
+		partial void autostart (Foundation.NSObject sender);
+
+		[Action ("quit:")]
+		partial void quit (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (autostartitem != null) {
+				autostartitem.Dispose ();
+				autostartitem = null;
+			}
+		}
 	}
 }
-
